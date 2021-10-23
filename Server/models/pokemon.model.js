0bbcb412bca15bff69f6977a701dd00df537fc4e
Elodie <85125911+Elodiemllr@users.noreply.github.com>
2021-10-23 22:32:01 +0200

    creation d'un model

--- Server/models/pokemon.model.js
+++ Server/models/pokemon.model.js
@@ -1,1 +1,36 @@
 // Les models nous permettent de voir a quoi va ressembler la BDD de pokemon (les infos qu'on va recevoir lors de notre requêtes etc)
+const mongoose = require("mongoose");
+
+//On déclare ce qui sera dans notre "Schéma" d'un pokemon
+const pokemonSchema = new mongoose.Schema(
+    {
+        pokemonId: {
+            type: String,
+            required: true,
+        },
+
+        name: {
+            type: String,
+            required: true,
+        },
+
+        picture: {
+            type: String,
+        },
+
+        abilities: {
+            type: [
+                {
+                    abilityId: String,
+                    abilityName: String,
+                },
+            ],
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+export default pokemonSchema;
